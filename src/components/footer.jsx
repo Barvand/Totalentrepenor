ed8fa57601a6314e76483657a10a9d3a8f8b59f6
B <128028346+Barvand@users.noreply.github.com>
2025-01-06 10:42:53 +0100

    updated responsiveness and added new section

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,26 +1,26 @@
 export default function Footer() {
   return (
-    <footer className="bg-black p-3">
-      <div className="container grid grid-cols-3 gap-2 place-content-evenly">
-        <div className="grid-col-1">
-          <h3 className="">Kontakt</h3>
-          <p>Lægdene 11, 5096, Bergen</p>
-          <p>40580085</p>
-          <p> Post@kgn.no </p>
-          <p>Org nr: 933886328 MVA</p>
+    <footer className="bg-black p-5">
+      <div className="container flex justify-between flex-wrap gap-8">
+        <div className="flex flex-col gap-2">
+          <h3 className="text-accent text-xl">Kontakt</h3>
+          <p className="text-sm">Lægdene 11, 5096, Bergen</p>
+          <p className="text-sm">40580085</p>
+          <p className="text-sm"> Post@kgn.no </p>
+          <p className="text-sm">Org nr: 933886328 MVA</p>
         </div>
-        <div className="grid-col-1">
-          <h3 className="">Tjenester</h3>
-          <p>Lægdene 11, 5096, Bergen</p>
-          <p>40580085</p>
-          <p> Post@kgn.no </p>
-          <p>Org nr: 933886328 MVA</p>
+        <div className="flex flex-col gap-2">
+          <h3 className="text-accent text-xl">Tjenester</h3>
+          <p className="text-sm">Lægdene 11, 5096, Bergen</p>
+          <p className="text-sm">40580085</p>
+          <p className="text-sm"> Post@kgn.no </p>
+          <p className="text-sm">Org nr: 933886328 MVA</p>
         </div>
-        <div className="grid-col-1">
-          <h3 className="">Om oss</h3>
-          <p>Lægdene 11, 5096, Bergen</p>
-          <p>40580085</p>
-          <p>Org nr: 933886328 MVA</p>
+        <div className="flex flex-col gap-1">
+          <h3 className="text-accent text-xl">Om oss</h3>
+          <p className="text-sm">Lægdene 11, 5096, Bergen</p>
+          <p className="text-sm">40580085</p>
+          <p className="text-sm">Org nr: 933886328 MVA</p>
         </div>
       </div>
     </footer>
--- src/components/homepage/sectionTwo.jsx
+++ src/components/homepage/sectionTwo.jsx
@@ -1,9 +1,37 @@
+import { AiOutlineBulb } from "react-icons/ai";
+import { FaPencilAlt } from "react-icons/fa";
+import { FaTools } from "react-icons/fa";
+
 export default function SectionTwo() {
   return (
-    <div className="bg-secondary w-100 h-100">
-      <div className="container">
-        <h2> Best guy in the world </h2>
+    <section className="bg-secondary w-full py-10 flex justify-center items-center h-96">
+      <div className="container flex justify-evenly flex-wrap gap-8">
+        <div className="text-center flex flex-col items-center gap-3 max-w-xs">
+          <FaTools className="text-5xl text-primary" />
+          <h2 className="text-xl font-bold">Vi har lang og divers erfaring</h2>
+          <p className="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="text-center flex flex-col items-center gap-3 max-w-xs">
+          <AiOutlineBulb className="text-5xl text-primary" />
+          <h2 className="text-xl font-bold">Pålitelighet</h2>
+          <p className="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="text-center flex flex-col items-center gap-3 max-w-xs">
+          <FaPencilAlt className="text-5xl text-primary" />
+          <h2 className="text-xl font-bold">Ingeniørkompetanse</h2>
+          <p className="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
+
